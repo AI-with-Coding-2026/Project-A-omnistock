d76c8629ac92471f18fa4f1b226698088d6bfc27
Enzu1997 <enzup29@gmail.com>
2026-08-26 12:58:10 +0200

    chore: switch to SQLite for Azure demo (no MySQL server needed)

--- omnistock/settings.py
+++ omnistock/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'omnistock.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='omnistock'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
