f84b3b4ffc95cdc5e4ac4dca1e9e9c2b07b2f830
Salman Khan <skbkhan31@gmail.com>
2026-08-27 19:20:14 +0500

    fix(orders): match urls.py formatting to main

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+
 from . import views
+
+
 urlpatterns = [
     path("", views.order_list, name="order_list"),
     path("index/", views.order_index, name="order_index"),
