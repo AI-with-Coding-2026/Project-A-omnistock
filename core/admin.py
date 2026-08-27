725ff540c8d9d2f0d4595dc8cdb710000963ec39
Enzu1997 <enzup29@gmail.com>
2026-08-27 15:37:58 +0200

    fix: configure roles and password management in admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role', 'is_active', 'is_staff', 'is_superuser')
-    list_filter = ('role', 'is_staff', 'is_superuser')
-    search_fields = ('username', 'email')
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            "OmniStock role",
+            {
+                "fields": ("role",),
+            },
+        ),
+    )
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (
+            "OmniStock role",
+            {
+                "fields": ("role",),
+            },
+        ),
+    )
